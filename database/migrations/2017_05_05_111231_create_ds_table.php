3a0f7d2cdfae60188e9398cf38158cd004230e52
Shoaziz <shoazizbakhtiyorov@mail.ru>
2019-05-05 20:33:32 +0500

    added migrations

--- database/migrations/2017_05_05_111231_create_ds_table.php
+++ database/migrations/2017_05_05_111231_create_ds_table.php
@@ -1,23 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDsTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ds', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -26,6 +21,5 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
     }
 }
